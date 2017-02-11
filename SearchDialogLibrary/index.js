36f64ed6c4f592f9ae4ba68fa3a8af9a0fec1cd6
SV <sv@SVs-MacBook-Air.local>
2017-02-11 15:31:54 +0100

    new: bye keyword to stop

--- SearchDialogLibrary/index.js
+++ SearchDialogLibrary/index.js
@@ -50,24 +50,15 @@ function create(settings) {
                 });
             }
 
-            var refining = !!args.refining;
-            if (refining) {
-                // D. returning from refine dialog
-                if (args.refiner && args.refiner.key) {
-                    session.send('Filtering by %s: %s', args.refiner.key, args.refiner.value);
-                }
-
-                return performSearch(session, query, selection);
-            }
-
             var input = args.response;
             var hasInput = typeof input === 'string';
             if (hasInput) {
-                // Process input
-                if (settings.multipleSelection && input.trim().toLowerCase() === 'list') {
-                    // E. List items
-                    listAddedItems(session);
-                    searchPrompt(session);
+                if (input.trim().toLowerCase() === 'bye') {
+                    // Say bye and shut up!
+                    var reply = new builder.Message(session)
+                        .text('Cool, just let me know when you need my help. Have a nice day! ');
+                    session.send(reply);
+                    // session.beginDialog('/');
                 } else {
                     // F. Perform search
                     var newQuery = Object.assign({}, query, { searchText: input });
@@ -105,7 +96,7 @@ function create(settings) {
 
                 // Restart dialog thread
                 // session.dialogData.firstTimeDone = true;
-                session.beginDialog('/');
+                session.replaceDialog('/');
                 // session.beginDialog('confirm-continue', { });
                 // session.replaceDialog('confirm-continue', {
                 //     message: 'Would you like to search again?',
@@ -117,115 +108,6 @@ function create(settings) {
 
             }));
 
-    // Handle refine search
-    library.dialog('refine', [
-        (session, args, next) => {
-            // args: query, selection, refiner(optional), prompt(optional)
-            var query = args.query || emptyQuery();
-            var selection = args.selection || [];
-            var refiner = args.refiner;
-            var prompt = args.prompt;
-
-            session.dialogData.query = query;
-            session.dialogData.selection = selection;
-
-            // Manual call
-            if (refiner) {
-                return next({
-                    response: { entity: refiner },
-                    prompt: prompt
-                });
-            }
-
-            // 1. Display choices from usable refiners
-            var usedRefiners = query.filters.map(s => s.key);
-            var usableRefiners = _.filter(settings.refiners, (r) => usedRefiners.indexOf(r) === -1);
-
-            if (usableRefiners.length === 0) {
-                // no more refiners
-                session.send('Oops! You used all the available refiners and you cannot refine the results anymore.');
-                session.endDialogWithResult({ query, selection });
-            } else {
-                // format them as { 'Label #1': 'value_1', 'Label #2'... }
-                var options = settings.refineFormatter(usableRefiners);
-                // add cancel option
-                options = Object.assign(_.zipObject([CancelOption], [CancelOption]), options);
-                builder.Prompts.choice(session, 'What do you want to refine by?', options);
-            }
-        },
-        (session, args, next) => {
-            // format refiners again and get value based on response
-            var selectedOption = args.response.entity;
-            if(selectedOption === CancelOption) {
-                // continue and cancel
-                return next();
-            }
-
-            var options = settings.refineFormatter(settings.refiners);
-            var refiner = options[selectedOption];
-            session.dialogData.refiner = refiner;
-
-            // 2. Search using current query and new refiner
-            var newQuery = Object.assign(
-                { facets: [refiner] },
-                session.dialogData.query);
-
-            settings.search(newQuery).then((response) => {
-                builder.Prompts.text(session, "Hi");
-                // 3. Display choices with refiner values length(response.facets)
-                // var facet = _.find(response.facets, ['key', refiner]);
-                // if (facet) {
-                //     var options = [CancelOption].concat(facet.options.map(formatRefinerOption));
-                //     var prompt = args.prompt || 'Here\'s what I found for ' + refiner + ' (select \'cancel\' if you don\'t want to select any of these):';
-                //     builder.Prompts.choice(session, prompt, options);
-                // } else {
-                //     // No results for selected facet, continue and cancel
-                //     next();
-                // }
-            });
-        },
-        (session, args, next) => {
-            // 4. Process refiner value selection
-            var query = session.dialogData.query;
-            var selection = session.dialogData.selection;
-            var refiner = session.dialogData.refiner;
-
-            args = args || {};
-            args.response = args.response || { entity: CancelOption };
-            var selectedValue = args.response.entity;
-            if (selectedValue === CancelOption) {
-                // Cancel
-                session.endDialogWithResult({ query, selection });
-            } else {
-                var refinerValue = parseRefinerValue(selectedValue);
-                // Returning dialog handles searching with current SearchTerm and new filters
-                var newQuery = applyRefiner(query, refiner, refinerValue);
-                session.endDialogWithResult({
-                    query: newQuery,
-                    selection,
-                    refining: true,
-                    refiner: { key: refiner, value: refinerValue }
-                });
-            }
-        }
-    ]);
-
-    // Helpers
-    library.dialog('confirm-continue', new builder.SimpleDialog((session, args) => {
-        args = args || {};
-        if (args.response === undefined) {
-            session.dialogData.selection = args.selection;
-            session.dialogData.query = args.query;
-            builder.Prompts.confirm(session, args.message || 'Do you want to continue searching?');
-        } else {
-            return session.endDialogWithResult({
-                done: !args.response,
-                selection: session.dialogData.selection,
-                query: session.dialogData.query
-            });
-        }
-    }));
-
     function performSearch(session, query, selection) {
 
         var reply = new builder.Message(session)
@@ -236,12 +118,18 @@ function create(settings) {
             console.log('Search.Output:', response);
             var results = response.facets;
             if (results.length === 0) {
+                // Report No Results and continue
+                var reply = new builder.Message(session)
+                    .text('Sorry, I didn\'t find any matches.');
+                session.send(reply);
+                session.beginDialog('/');
+
                 // No Results - Prompt retry
-                session.beginDialog('confirm-continue', {
-                    message: 'Sorry, I didn\'t find any matches. Do you want to retry your search?',
-                    selection: selection,
-                    query: query
-                });
+                // session.beginDialog('confirm-continue', {
+                //     message: 'Sorry, I didn\'t find any matches. Do you want to retry your search?',
+                //     selection: selection,
+                //     query: query
+                // });
             } else {
                 // Handle results selection
                 session.beginDialog('results', {
@@ -281,45 +169,24 @@ function create(settings) {
         return card;
     }
 
-    function applyRefiner(query, refiner, refinerValue) {
-        query.filters.push({ key: refiner, value: refinerValue });
-        query.pageNumber = 1;
-        return query;
-    }
-
-    function formatRefinerOption(facet) {
-        return util.format('%s (%d)', facet.value, facet.count);
-    }
-
-    function parseRefinerValue(s) {
-        return s.split('(')[0].trim();
-    }
+    // const CancelOption = 'Bye';
 
     function searchPrompt(session) {
         // var prompt = 'Hello! What would you like to search for? For example, "wien ticket"';
+        // give keyword to search
         var prompt = 'What would you like to search for?';
-        if (session.dialogData.firstTimeDone) {
-            prompt = 'What else would you like to search for?';
-            // if (settings.multipleSelection) {
-            //     prompt += ' You can also *list* all items you\'ve added so far.';
-            // }
-        }
-        session.dialogData.firstTimeDone = true;
+        // if (session.dialogData.firstTimeDone) {
+        //     prompt = 'What else would you like to search for?';
+        //     // if (settings.multipleSelection) {
+        //     //     prompt += ' You can also *list* all items you\'ve added so far.';
+        //     // }
+        // }
+        // session.dialogData.firstTimeDone = true;
+        // var options = [CancelOption];
+        // var prompt = args.prompt || 'Here\'s what I found for ' + refiner + ' (select \'cancel\' if you don\'t want to select any of these):';
         builder.Prompts.text(session, prompt);
-    }
 
-    function listAddedItems(session) {
-        var selection = session.dialogData.selection || [];
-        if (selection.length === 0) {
-            session.send('You have not added anything yet.');
-        } else {
-            var actions = selection.map((hit) => builder.CardAction.imBack(session, hit.title));
-            var message = new builder.Message(session)
-                .text('Here\'s what you\'ve added to your list so far:')
-                .attachments(selection.map(searchHitAsCard.bind(null, false)))
-                .attachmentLayout(builder.AttachmentLayout.list);
-            session.send(message);
-        }
+        // builder.Prompts.choice(session, prompt, options);
     }
 
     function emptyQuery() {
@@ -331,10 +198,6 @@ function create(settings) {
 
 function begin(session, args) {
     session.beginDialog('search:/', args);
-}
-
-function refine(session, args) {
-    session.beginDialog('search:refine', args);
 }
 
 // This helper transforms each of the AzureSearch result items using the mapping function provided (itemMap) 
@@ -352,6 +215,5 @@ function defaultResultsMapper(itemMap) {
 module.exports = {
     create: create,
     begin: begin,
-    refine: refine,
     defaultResultsMapper: defaultResultsMapper
 };
